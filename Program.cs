db4b855dedf7d1959e13a525a9b33c9955258668
KolesnikovPavel <KolesnikovPavel32@gmail.com>
2019-08-06 16:39:43 +0500

    Program_version_1

--- Program.cs
+++ Program.cs
@@ -7,12 +7,6 @@ namespace stage_parser
 {
     public class Program
     {
-        public static void FloorWriter (string line, string newCsvPath)
-        {
-            using (var sw = new StreamWriter(newCsvPath, true))
-                sw.WriteLine(line);
-        }
-
         public static void Main()
         {
             int offerCounter = 0;
@@ -24,17 +18,21 @@ namespace stage_parser
                 var offer = db.Offers.Where(x => x.raw_floor_level.HasValue).ToList();
                 foreach (var e in offer)
                 {
-                    //Console.WriteLine($"{e.raw_floor_level}");
-                    //Console.Write($"{e.description}\n\n");
                     offerCounter++;
-                    int floor_level = 2; //тут нужно передать description из БД в CommentStageParser и получить значение этажа
-                    if (e.raw_floor_level != floor_level)
-                        Console.WriteLine("тест не пройден!\n {0} \nОжидался вывод: {1}, вместо {2}\n", 
-                            e.description, e.raw_floor_level, floor_level, errorCounter++);
+                    var floor_level = new CommentStageParser(e.description);
+                    int number;
+                    if (Int32.TryParse(floor_level.GetParserResult(), out number))
+                    {
+                        if (e.raw_floor_level != number)
+                            Console.WriteLine("тест не пройден\n{0}\nОжидался вывод: {1}, вместо {2}\n",
+                                e.description, e.raw_floor_level, floor_level.GetParserResult(), errorCounter++);
+                    }
+                    else
+                        Console.WriteLine("Парсер вернул не число");
                 }
             }
             Console.WriteLine("\nВсего тестов: {0}", offerCounter);
-            Console.WriteLine("Тестов не пройдено: {0}",errorCounter);
+            Console.WriteLine("Тестов не пройдено: {0}", errorCounter);
         }
     }
 }
