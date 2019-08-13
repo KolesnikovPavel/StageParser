fc83348dee517b292c5b6d057a6e6d5aab9aa5f0
KolesnikovPavel <KolesnikovPavel32@gmail.com>
2019-08-13 15:48:05 +0500

    some more stuff

--- Program.cs
+++ Program.cs
@@ -6,105 +6,78 @@ namespace stage_parser
 {
     public class Program
     {
-        public static string ConvertEnglishLetters(string sentence)
+        public static string ConvertEnglishLetters(string description)
         {
             string[] engLetters = new string[] { "A", "a", "E", "e", "T", "Y", "y", "O", "o", "P", "p", "H", "K", "k", "X", "x", "C", "c", "B", "b", "N", "n", "M", "m" };
             string[] ruLetters = new string[] { "А", "а", "Е", "е", "Т", "У", "у", "О", "о", "Р", "р", "Н", "К", "к", "Х", "х", "С", "с", "В", "ь", "И", "п", "М", "м" };
             for (int i = 0; i < engLetters.Length; i++)
-                if (sentence.Contains(engLetters[i]))
-                    sentence = sentence.Replace(engLetters[i], ruLetters[i]);
-            return sentence;
+                if (description.Contains(engLetters[i]))
+                    description = description.Replace(engLetters[i], ruLetters[i]);
+            return description;
         }
 
-        public static bool MoreThanOneFloor(stage_parser.Offer e)
+        public static bool MoreThanOneFloor(stage_parser.Offer offer)
         {
-            return e.Multilevel_floor.HasValue && !e.raw_floor_level.HasValue ? true : false;
+            return offer.Multilevel_floor.HasValue && !offer.raw_floor_level.HasValue ? true : false;
         }
 
-        public static void DisplayTestResult (stage_parser.Offer e, int number)
+        public static bool DatabaseHasFilledValues(stage_parser.Offer offer)
+        {
+            return offer.raw_floor_level.HasValue || offer.Multilevel_floor.HasValue ? true : false;
+        }
+
+        public static void DisplayTestResult (stage_parser.Offer offer, int parser_floor_level)
         {
             Console.WriteLine("Тест не пройден. id: {0}\n{1}\n Ожидался этаж: {2}, вместо {3}\n",
-                e.id, e.description, e.raw_floor_level, number);
+                offer.id, offer.description, offer.raw_floor_level, parser_floor_level);
+        }
+        
+        public static void CompareAndDisplayResult(stage_parser.Offer offer, int parser_floor_level)
+        {
+            if (offer.raw_floor_level != parser_floor_level)
+                DisplayTestResult(offer, parser_floor_level);
         }
 
-        public static void DisplayCounters(int offerCounter, int changedValues, int singleFloorCounter, int multiFloorCounter, int noValueCounter, int errorCounter)
+        public static int DisplayHowParserResultChanged(stage_parser.Offer offer, int parser_floor_level, int changedValues)
         {
-            Console.WriteLine("\nВсего тестов: {0}", offerCounter);
-            Console.WriteLine("Значений изменилось: {0}", changedValues);
-            Console.WriteLine("Тесты для одноэтажных помещений: {0}", singleFloorCounter);
-            Console.WriteLine("Тесты для мультиэтажных помещений: {0}", multiFloorCounter);
-            Console.WriteLine("Количество тестов без значения: {0}", noValueCounter);
-            Console.WriteLine("Тестов не пройдено: {0}", errorCounter);
-        }
-
-        public static int DisplayHowParserResultChanged (stage_parser.Offer e, int number, int changedValues)
-        {
-            if (e.floor_level != number && number != e.raw_floor_level)
-                Console.WriteLine("id: <{0}> было: {1} стало: {2} должно {3}",
-                    e.id, e.floor_level, number, e.raw_floor_level, changedValues++);
+            if (offer.floor_level != parser_floor_level && parser_floor_level != offer.raw_floor_level && offer.Multilevel_floor != 0)
+                Console.WriteLine("id: <{0}> было: {1} стало: {2} должно: {3}",
+                    offer.id, offer.floor_level, parser_floor_level, offer.raw_floor_level, changedValues++);
             return changedValues;
         }
-
 
         public static void Main()
         {
             int offerCounter = 0;
-            int singleFloorCounter = 0;
-            int multiFloorCounter = 0;
-            int errorCounter = 0;
-            int noValueCounter = 0;
             int changedValues = 0;
-
+            string checkMultilevel = "нет";
             using (OfferContext db = new OfferContext())
             {
-                Console.WriteLine("Включить тесты для мультиэтажных помещений? (да/нет)");
-                string multilevel = Console.ReadLine();
-                Console.WriteLine("Посмотреть как изменились значения? (да/нет)");
-                string test = Console.ReadLine();
-                Console.Clear();
-
-                var offer = db.Offers.Where(x => (x.raw_floor_level.HasValue || x.Multilevel_floor.HasValue) /*&& x.id == 2410*/).ToList();
-                foreach (var e in offer)
+                var offers = db.Offers.Where(offer => /*(DatabaseHasFilledValues(offer)) &&*/ offer.id < 100).ToList();
+                foreach (var offer in offers)
                 {
-                    var floor_level = new CommentStageParser(ConvertEnglishLetters(e.description));
-                    if (Int32.TryParse(floor_level.GetParserResult(), out int number))
+                    offerCounter++;
+                    var commentStageParser = new CommentStageParser(ConvertEnglishLetters(offer.description));
+                    if (Int32.TryParse(commentStageParser.GetParserResult(), out int parser_floor_level))
                     {
-                        if (test == "да")
-                        {
-                            if (number < 163)
-                                changedValues = DisplayHowParserResultChanged(e, number, changedValues);
-                        }
-                        else
-                        {
-                            if (MoreThanOneFloor(e))
-                            {
-                                if (multilevel == "да")
-                                {
-                                    DisplayTestResult(e, number);
-                                    multiFloorCounter++;
-                                    errorCounter++;
-                                    offerCounter++;
-                                }
-                            }
-                            else
-                            {
-                                offerCounter++;
-                                singleFloorCounter++;
-                                if (e.raw_floor_level != number)
-                                {
-                                    DisplayTestResult(e, number);
-                                    errorCounter++;
-                                }
-                            }
-                        }
+                        //changedValues = DisplayHowParserResultChanged(offer, parser_floor_level, changedValues);
+                        //if (MoreThanOneFloor(offer))
+                        //{
+                        //    if (checkMultilevel != "нет")
+                        //        DisplayTestResult(offer, parser_floor_level);
+                        //}
+                        //else
+                        //{
+                        //    CompareAndDisplayResult(offer, parser_floor_level);
+                        //}
                     }
-                    else
-                        if (test != "да")
-                            Console.WriteLine("Парсер вернул не число. id <{0}>\n{1}\n",
-                                e.id, e.description, errorCounter++, noValueCounter++, offerCounter++);
+                    //else
+                    //    Console.WriteLine("Парсер вернул не число. id <{0}>\n{1}\n",
+                    //        offer.id, offer.description);
                 }
             }
-            DisplayCounters(offerCounter, changedValues, singleFloorCounter, multiFloorCounter, noValueCounter, errorCounter);
+            Console.WriteLine("\nВсего тестов " + offerCounter);
+            Console.WriteLine("Тестов изменилось " + changedValues);
         }
     }
 }
